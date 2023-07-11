1e3d44fe0bb679d4655e1db29b4e6b62df76da55
内核炸了啊对对队 <jtzhpf@126.com>
2023-07-11 23:21:43 +0800

    fix crosstool-ng support

--- crosstool-ng-git.sh
+++ crosstool-ng-git.sh
@@ -4,6 +4,13 @@ set -ex
 URL=https://github.com/crosstool-ng/crosstool-ng
 REV=943364711a650d9b9e84c1b42c91cc0265b6ab5c
 
+if [ ! -d "crosstool-ng" ]; then
+    mkdir "crosstool-ng"
+    echo "Created directory 'crosstool-ng'"
+else
+    rm -rf crosstool-ng
+    mkdir crosstool-ng
+fi
 mkdir crosstool-ng
 cd crosstool-ng
 git init
